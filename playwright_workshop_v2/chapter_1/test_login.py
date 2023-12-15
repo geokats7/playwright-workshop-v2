e3d7c1553def6943e6654a1daa3c476830bff307
George Katsaros <katsaros@orfium.com>
2023-12-15 11:56:04 +0200

    fix: refactor files

--- playwright_workshop_v2/chapter_1/test_login.py
+++ playwright_workshop_v2/chapter_1/test_login.py
@@ -1,25 +1,13 @@
-import pytest
-from playwright.sync_api import sync_playwright
+def test_login_successful(page):
+    page.goto("https://practicetestautomation.com/practice-test-login/")
+    page.locator("#username").fill("student")
+    page.locator("#password").fill("Password123")
+    page.locator("#submit").click()
+    assert page.locator("text=Logged In Successfully").is_visible()
 
-
-def test_login_successful():
-    with sync_playwright() as page:
-        browser = page.chromium.launch(headless=False)
-        context = browser.new_context()
-        page = context.new_page()
-        page.goto("https://practicetestautomation.com/practice-test-login/")
-        page.locator("#username").fill("student")
-        page.locator("#password").fill("Password123")
-        page.locator("#submit").click()
-        assert page.locator("text=Logged In Successfully").is_visible()
-
-def test_login_wrong_username():
-    with sync_playwright() as page:
-        browser = page.chromium.launch(headless=False)
-        context = browser.new_context()
-        page = context.new_page()
-        page.goto("https://practicetestautomation.com/practice-test-login/")
-        page.locator("#username").fill("wrong_username")
-        page.locator("#password").fill("Password123")
-        page.locator("#submit").click()
-        assert page.locator("#error").inner_text() == "Your username is invalid!"
+def test_login_wrong_username(page):
+    page.goto("https://practicetestautomation.com/practice-test-login/")
+    page.locator("#username").fill("wrong_username")
+    page.locator("#password").fill("Password123")
+    page.locator("#submit").click()
+    assert page.locator("#error").inner_text() == "Your username is invalid!"
